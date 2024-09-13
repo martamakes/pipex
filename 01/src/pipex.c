8489d9cdaec06b92fc689655f416016400f13b2e
Marta Vigara Gonzalez <mvigara-@c1r2s6.42madrid.com>
2024-09-13 23:06:45 +0200

    works

--- 01/src/pipex.c
+++ 01/src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: mvigara- <mvigara-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 15:55:24 by mvigara           #+#    #+#             */
-/*   Updated: 2024/09/13 21:56:50 by mvigara-         ###   ########.fr       */
+/*   Updated: 2024/09/13 22:51:52 by mvigara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,24 +20,22 @@ void	child_process(char **argv, char **envp, int *pipe_fd)
 	full_path = realpath(argv[1], NULL);
 	if (!full_path)
 	{
-		ft_putstr_fd("Error resolving input file path\n", 2);
+		ft_putstr_fd("no such file or directory\n", 2);
 		exit(EXIT_FAILURE);
 	}
 	infile = open(full_path, O_RDONLY);
 	free(full_path);
 	if (infile == -1)
 	{
-		ft_putstr_fd("Error opening input file\n", 2);
+		ft_putstr_fd("no such file or directory\n", 2);
 		exit(EXIT_FAILURE);
 	}
-	ft_putstr_fd("Child process: Input file opened successfully\n", 2);
 	if (dup2(infile, STDIN_FILENO) == -1 || dup2(pipe_fd[1], STDOUT_FILENO) ==
 		-1)
 		handle_error();
 	close(pipe_fd[0]);
 	close(pipe_fd[1]);
 	close(infile);
-	ft_putstr_fd("Child process: About to execute command\n", 2);
 	execute_pipex(argv[2], envp);
 }
 
@@ -49,24 +47,22 @@ void	parent_process(char **argv, char **envp, int *pipe_fd)
 	full_path = get_outfile_path(argv[4]);
 	if (!full_path)
 	{
-		ft_putstr_fd("Error getting output file path\n", 2);
+		ft_putstr_fd("no such file or directory\n", 2);
 		exit(EXIT_FAILURE);
 	}
 	outfile = open(full_path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	free(full_path);
 	if (outfile == -1)
 	{
-		ft_putstr_fd("Error opening output file\n", 2);
+		ft_putstr_fd("no such file or directory\n", 2);
 		exit(EXIT_FAILURE);
 	}
-	ft_putstr_fd("Parent process: Output file opened successfully\n", 2);
 	if (dup2(pipe_fd[0], STDIN_FILENO) == -1 || dup2(outfile, STDOUT_FILENO) ==
 		-1)
 		handle_error();
 	close(pipe_fd[0]);
 	close(pipe_fd[1]);
 	close(outfile);
-	ft_putstr_fd("Parent process: About to execute command\n", 2);
 	execute_pipex(argv[3], envp);
 }
 
@@ -87,18 +83,10 @@ void	execute_pipex(char *argv, char **envp)
 		free_matrix(cmd);
 		exit(EXIT_FAILURE);
 	}
-	ft_putstr_fd("Executing command: ", 2);
-	ft_putstr_fd(cmd[0], 2);
-	ft_putstr_fd("\n", 2);
-	ft_putstr_fd("About to execve: ", 2);
-	ft_putstr_fd(path, 2);
-	ft_putstr_fd("\n", 2);
 	if (execve(path, cmd, envp) == -1)
 	{
-		ft_putstr_fd("Error executing command: ", 2);
+		ft_putstr_fd("Error: Command not found: ", 2);
 		ft_putstr_fd(cmd[0], 2);
-		ft_putstr_fd("\n", 2);
-		ft_putstr_fd(strerror(errno), 2);
 		ft_putstr_fd("\n", 2);
 		free_matrix(cmd);
 		free(path);
@@ -127,23 +115,8 @@ void	setup_pipes_and_execute(char **argv, char **envp)
 		parent_process(argv, envp, pipe_fd);
 	close(pipe_fd[0]);
 	close(pipe_fd[1]);
-	ft_putstr_fd("Parent: Waiting for child 1\n", 2);
 	waitpid(pid1, &status, 0);
-	if (WIFEXITED(status))
-		ft_printf("Child 1 exited with status %d\n", WEXITSTATUS(status));
-	else if (WIFSIGNALED(status))
-		ft_printf("Child 1 killed by signal %d\n", WTERMSIG(status));
-	else
-		ft_putstr_fd("Child 1 exited abnormally\n", 2);
-	ft_putstr_fd("Parent: Waiting for child 2\n", 2);
 	waitpid(pid2, &status, 0);
-	if (WIFEXITED(status))
-		ft_printf("Child 2 exited with status %d\n", WEXITSTATUS(status));
-	else if (WIFSIGNALED(status))
-		ft_printf("Child 2 killed by signal %d\n", WTERMSIG(status));
-	else
-		ft_putstr_fd("Child 2 exited abnormally\n", 2);
-	ft_putstr_fd("Parent: Both children have finished\n", 2);
 }
 int	main(int argc, char **argv, char **envp)
 {
--- 01/src/utils.c
+++ 01/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: mvigara- <mvigara-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 12:14:37 by mvigara           #+#    #+#             */
-/*   Updated: 2024/09/13 22:11:09 by mvigara-         ###   ########.fr       */
+/*   Updated: 2024/09/13 23:04:43 by mvigara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,18 @@ void	check_empty_args(int argc, char **argv)
 	{
 		if (ft_strlen(argv[i]) == 0)
 		{
-			ft_putstr_fd("command not found: ", 2);
-			ft_putstr_fd(argv[i], 2);
-			ft_putstr_fd("\n", 2);
+			if (i == 1 || i == 4)
+			{
+				ft_putstr_fd("no such file or directory: ", 2);
+				ft_putstr_fd(argv[i], 2);
+				ft_putstr_fd("\n", 2);
+			}
+			else
+			{
+				ft_putstr_fd("command not found: ", 2);
+				ft_putstr_fd(argv[i], 2);
+				ft_putstr_fd("\n", 2);
+			}
 			exit(EXIT_FAILURE);
 		}
 		i++;
@@ -32,8 +41,7 @@ void	check_empty_args(int argc, char **argv)
 
 void	handle_error(void)
 {
-	ft_putstr_fd(strerror(errno), 2);
-	ft_putstr_fd("\n", 2);
+	perror("Error");
 	exit(EXIT_FAILURE);
 }
 
