5c979a7a37846ef0a26926a1d4b6f6e7ca18dcff
Marta Vigara Gonzalez <mvigara-@c1r2s4.42madrid.com>
2024-09-13 15:03:44 +0200

    modified bonus

--- inc/pipex_bonus.h
+++ inc/pipex_bonus.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex_bonus.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marta <marta@student.42.fr>                +#+  +:+       +#+        */
+/*   By: mvigara- <mvigara-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 18:37:46 by mvigara-          #+#    #+#             */
-/*   Updated: 2024/09/12 20:49:28 by marta            ###   ########.fr       */
+/*   Updated: 2024/09/13 14:52:02 by mvigara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,23 @@
 
 typedef struct s_pipex
 {
-    int argc;
-    char **argv;
-    int here_doc;
-    int start_idx;
-    int input_fd;
-    int output_fd;
-} t_pipex;
+	int		argc;
+	char	**argv;
+	int		here_doc;
+	int		start_idx;
+	int		input_fd;
+	int		output_fd;
+}			t_pipex;
 
-void    execute_cmd(char *cmd, char **envp);
-void    child_process(char *cmd, char **envp, int input_fd, int output_fd);
-void    here_doc(char *limiter, int pipe_fd[2]);
-void    handle_error(void);
-char    *find_path(char *cmd, char **envp);
-void    free_matrix(char **matrix);
-
-// New functions in pipex_bonus_utils.c
-void    init_pipex(t_pipex *pipex, int argc, char **argv);
-void    setup_here_doc(t_pipex *pipex, char **argv);
-void    setup_files(t_pipex *pipex, char **argv);
-void    pipex_process(int argc, char **argv, char **envp, t_pipex *pipex);
+void		execute_cmd(char *cmd, char **envp);
+void		child_process(char *cmd, char **envp, int input_fd, int output_fd);
+void		here_doc(char *limiter, int pipe_fd[2]);
+void		handle_error(void);
+char		*find_path(char *cmd, char **envp);
+void		free_matrix(char **matrix);
+void		init_pipex(t_pipex *pipex, int argc, char **argv);
+void		setup_here_doc(t_pipex *pipex, char **argv);
+void		setup_files(t_pipex *pipex, char **argv);
+void		pipex_process(int argc, char **argv, char **envp, t_pipex *pipex);
 
 #endif
--- src/pipex_bonus.c
+++ src/pipex_bonus.c
@@ -3,37 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   pipex_bonus.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marta <marta@student.42.fr>                +#+  +:+       +#+        */
+/*   By: mvigara- <mvigara-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 18:40:21 by mvigara           #+#    #+#             */
-/*   Updated: 2024/09/12 20:47:47 by marta            ###   ########.fr       */
+/*   Updated: 2024/09/13 14:40:47 by mvigara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
-    t_pipex pipex;
+	t_pipex	pipex;
 
-    if (argc < 5)
-    {
-        ft_printf("Error: Invalid number of arguments\n");
-        ft_printf("Usage: ./pipex file1 cmd1 cmd2 ... cmdn file2\n");
-        ft_printf("   or: ./pipex here_doc LIMITER cmd1 cmd2 ... cmdn file2\n");
-        exit(1);
-    }
-    init_pipex(&pipex, argc, argv);
-    if (ft_strncmp(argv[1], "here_doc", 8) == 0)
-        setup_here_doc(&pipex, argv);
-    else
-        setup_files(&pipex, argv);
-    if (pipex.input_fd == -1 || pipex.output_fd == -1)
-        handle_error();
-    dup2(pipex.input_fd, STDIN_FILENO);
-    dup2(pipex.output_fd, STDOUT_FILENO);
-    close(pipex.input_fd);
-    close(pipex.output_fd);
-    pipex_process(argc, argv, envp, &pipex);
-    return (0);
+	if (argc < 5)
+	{
+		ft_printf("Error: Invalid number of arguments\n");
+		ft_printf("Usage: ./pipex file1 cmd1 cmd2 ... cmdn file2\n");
+		ft_printf("   or: ./pipex here_doc LIMITER cmd1 cmd2 ... cmdn file2\n");
+		exit(1);
+	}
+	init_pipex(&pipex, argc, argv);
+	if (ft_strncmp(argv[1], "here_doc", 8) == 0)
+		setup_here_doc(&pipex, argv);
+	else
+		setup_files(&pipex, argv);
+	if (pipex.input_fd == -1 || pipex.output_fd == -1)
+		handle_error();
+	dup2(pipex.input_fd, STDIN_FILENO);
+	dup2(pipex.output_fd, STDOUT_FILENO);
+	close(pipex.input_fd);
+	close(pipex.output_fd);
+	pipex_process(argc, argv, envp, &pipex);
+	return (0);
 }
